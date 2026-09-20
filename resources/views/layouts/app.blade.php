37117ad03b03a3cff242802277b691dcfe4cfe7d
paizzt <martevenfaisal@gmail.com>
2026-09-20 14:41:42 +0800

    Update favicon to Arjuna One logo across all layouts

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,7 +7,7 @@
     <title>@yield('title') - App Absensi</title>
     
     <!-- Favicon -->
-    <link rel="icon" href="{{ asset('images/logo.png') }}" type="image/png">
+    <link rel="icon" href="{{ asset('images/logo-arjuna-one.png') }}" type="image/png">
     
     <link href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700&display=swap" rel="stylesheet">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
